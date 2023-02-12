eb54eddaf8d83db16eb5888a17af22b4a8d96f8c
chaimaeouardani <chaimaeouardani@gmail.com>
2023-02-12 14:10:34 +0100

    selected components are gotten in the run view + configuration of the admin page

--- backend_django/ternary_azeotrope/admin.py
+++ backend_django/ternary_azeotrope/admin.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 from .models import BinaryRelation, Component
 
 # Register your models here.
-
-
 admin.site.register(Component)
 admin.site.register(BinaryRelation)
