de6c40dc5cc69df560d358f3b0a6587ad2700aab
chaimaeouardani <chaimaeouardani@gmail.com>
2023-02-12 13:39:52 +0100

    chosen components are now gotten in the run views to be sent for diagram generation

--- backend_django/ternary_azeotrope/urls.py
+++ backend_django/ternary_azeotrope/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("/run", views.run, name="run"),
 ]
--- backend_django/ternary_azeotrope/views.py
+++ backend_django/ternary_azeotrope/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 from ternary_azeotrope.models import Component
 
 # Create your views here.
@@ -8,6 +7,20 @@ from ternary_azeotrope.models import Component
 
 def index(request):
     # return HttpResponse("Home page")
-    return render(request, 'ternary_azeotrope/index.html', {
-        "componenents": Component.objects.all()
-    })
+    return render(
+        request,
+        "ternary_azeotrope/index.html",
+        {"componenents": Component.objects.all()},
+    )
+
+
+def run(request):
+    if request.method == "POST":
+        component1 = Component.objects.get(pk=request.POST["component1"])
+        component2 = Component.objects.get(pk=request.POST["component2"])
+        component3 = Component.objects.get(pk=request.POST["component3"])
+
+        # line only for test, to comment after generating diagram view is done etc
+        return HttpResponse(
+            f"Chosen components : {component1},  {component2},  {component3}"
+        )
