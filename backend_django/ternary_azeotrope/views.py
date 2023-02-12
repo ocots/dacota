e7d936ecceb50e0daba1790b65cda5ba9885749a
chaimaeouardani <chaimaeouardani@gmail.com>
2023-02-12 17:33:02 +0100

    removing the database from gitignore + user not allowed to choose the same component

--- backend_django/ternary_azeotrope/views.py
+++ backend_django/ternary_azeotrope/views.py
@@ -10,17 +10,39 @@ def index(request):
     return render(
         request,
         "ternary_azeotrope/index.html",
-        {"componenents": Component.objects.all()},
+        {"componenents": Component.objects.all(), "valid_components": True},
     )
 
 
 def run(request):
     if request.method == "POST":
-        component1 = Component.objects.get(pk=request.POST["component1"])
-        component2 = Component.objects.get(pk=request.POST["component2"])
-        component3 = Component.objects.get(pk=request.POST["component3"])
+        try:
+            id1 = int(request.POST["component1"])
+            id2 = int(request.POST["component2"])
+            id3 = int(request.POST["component3"])
+            component1 = Component.objects.get(pk=id1)
+            component2 = Component.objects.get(pk=id2)
+            component3 = Component.objects.get(pk=id3)
 
-        # line only for test, to comment after generating diagram view is done etc
-        return HttpResponse(
-            f"Chosen components : {component1},  {component2},  {component3}"
-        )
+            if (
+                component1 == component2
+                or component1 == component3
+                or component2 == component3
+            ):
+                raise ValueError
+
+            # line only for test, to comment after generating diagram view is done etc
+            return HttpResponse(
+                f"Chosen components : {component1},  {component2},  {component3}"
+            )
+
+        except ValueError:
+            """return render(
+                request,
+                "ternary_azeotrope/index.html",
+                {"valid_components": False},
+            )"""
+
+            return HttpResponse(
+                "user didn't select 3 components or components are not distinct"
+            )
