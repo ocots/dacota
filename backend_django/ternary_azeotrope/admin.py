cebf50e819f4af0a16d8c6c16ea07b6dd3e7d87f
chaimaeouardani <chaimaeouardani@gmail.com>
2023-02-12 14:05:21 +0100

    admin page configured

--- backend_django/ternary_azeotrope/admin.py
+++ backend_django/ternary_azeotrope/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import BinaryRelation, Component
+
 # Register your models here.
+
+
+admin.site.register(Component)
+admin.site.register(BinaryRelation)
