90cfcfb12d4994bf416f34536c336366d30c1fe0
chaimaeouardani <chaimaeouardani@gmail.com>
2023-02-14 12:49:14 +0100

    table html squeleton + some css added by mahmoud

---
+++ backend_django/ternary_azeotrope/static/js/index.js
@@ -0,0 +1,16 @@
+
+/*const comp_form =  document.getElementsByName("form");
+console.log(comp_form);
+
+comp_form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("spinner").style.display = "block";
+    this.submit();
+});
+
+$('form').on('submit', function(){
+    console.log("submitted");
+});*/
+console.log("DEBUG")
+const submitButton = document.getElementById("submit-btn")
+console.log(submitButton)
