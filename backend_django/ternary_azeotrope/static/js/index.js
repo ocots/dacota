f953b5c4fc6e3d19f35b5eb3eed5a5b984291971
ihsane tami <ihsanetami2000@gmail.com>
2023-02-14 16:30:18 +0100

    menu in index

--- backend_django/ternary_azeotrope/static/js/index.js
+++ backend_django/ternary_azeotrope/static/js/index.js
@@ -1,4 +1,3 @@
-
 /*const comp_form =  document.getElementsByName("form");
 console.log(comp_form);
 
@@ -11,6 +10,22 @@ comp_form.addEventListener("submit", function(event) {
 $('form').on('submit', function(){
     console.log("submitted");
 });*/
-console.log("DEBUG")
-const submitButton = document.getElementById("submit-btn")
-console.log(submitButton)
+console.log("DEBUG");
+const submitButton = document.getElementById("submit-btn");
+console.log(submitButton);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const menu = document.getElementById("menu");
+  const content = document.getElementById("content");
+
+  menuIcon.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+
+  content.addEventListener("click", function () {
+    if (menu.classList.contains("show")) {
+      menu.classList.remove("show");
+    }
+  });
+});
