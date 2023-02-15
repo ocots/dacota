a9cbb30135750efe8c1c031626cf5902e9371430
ihsane tami <ihsanetami2000@gmail.com>
2023-02-16 00:01:44 +0100

    index.css

--- backend_django/ternary_azeotrope/static/js/menu.js
+++ backend_django/ternary_azeotrope/static/js/menu.js
@@ -1,22 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menu-icon");
   const menu = document.getElementById("menu");
-  const content = document.getElementById("content");
 
   menuIcon.addEventListener("click", function () {
     menu.classList.toggle("show");
   });
 
-  content.addEventListener("click", function () {
+  var menuToggleIcon = document.getElementById("menu-icons");
+
+  menuToggleIcon.addEventListener("click", function () {
     if (menu.classList.contains("show")) {
       menu.classList.remove("show");
     }
   });
-});
-var menuToggleIcon = document.getElementById("menu-icons");
-
-menuToggleIcon.addEventListener("click", function () {
-  if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
-  }
 });
