0058e539698716081e8fd3311eb6f7d377bf8801
Voyinno <42241036+Voyinno@users.noreply.github.com>
2023-02-22 17:31:03 +0100

    Update settings.py

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -24,6 +24,10 @@ MS_ENDPOINT = env("ENDPOINT")
 PORT = env("PORT")
 ALLOWED_HOST = env("ALLOWED_HOST")
 
+
+HOME = Path.home()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,7 +96,7 @@ WSGI_APPLICATION = "backend_django.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": HOME / "db.sqlite3",
     }
 }
 
